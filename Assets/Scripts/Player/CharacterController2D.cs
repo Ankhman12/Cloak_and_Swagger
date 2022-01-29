fc2fca8e507aa371550a00cc291e2333f6135763
Mitchell Dunning <ankhman12@gmail.com>
2022-01-28 20:58:34 -0500

    Added guard ai
    Implemented Patrolling State of AI, no transitions

--- Assets/Scripts/Player/CharacterController2D.cs
+++ Assets/Scripts/Player/CharacterController2D.cs
@@ -70,90 +70,88 @@ public class CharacterController2D : MonoBehaviour
 
 	public void Move(float move, bool crouch, bool sprint, bool jump, bool wasJumping)
 	{
-		// If crouching, check to see if the character can stand up
-		// If the character has a ceiling preventing them from standing up, keep them crouching
-		Collider2D col = Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround);
-		if (col != null)
-		{
-			//currentCeilingObject = col.gameObject;
-
-			if (!crouch && m_Grounded)
+			// If crouching, check to see if the character can stand up
+			// If the character has a ceiling preventing them from standing up, keep them crouching
+			Collider2D col = Physics2D.OverlapCircle(m_CeilingCheck.position, k_CeilingRadius, m_WhatIsGround);
+			if (col != null)
 			{
-				crouch = true;
-			}
-		}
+				//currentCeilingObject = col.gameObject;
 
-		//only control the player if grounded or airControl is turned on
-		if (m_Grounded || m_AirControl)
-		{
-
-			// If crouching
-			if (crouch)
-			{
-				if (!m_wasCrouching)
+				if (!crouch && m_Grounded)
 				{
-					m_wasCrouching = true;
-					OnCrouchEvent.Invoke(true);
-				}
-
-				// Reduce the speed by the crouchSpeed multiplier
-				move *= m_CrouchSpeed;
-
-				// Disable one of the colliders when crouching
-				if (m_CrouchDisableCollider != null)
-					m_CrouchDisableCollider.enabled = false;
-			}
-			else
-			{
-				// Enable the collider when not crouching
-				if (m_CrouchDisableCollider != null)
-					m_CrouchDisableCollider.enabled = true;
-
-				if (m_wasCrouching)
-				{
-					m_wasCrouching = false;
-					OnCrouchEvent.Invoke(false);
-				}
-
-				if (sprint && m_Grounded)
-				{
-					move *= m_SprintSpeed;
+					crouch = true;
 				}
 			}
 
-
-			// Move the character by finding the target velocity
-			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
-			// And then smoothing it out and applying it to the character
-			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
-
-			// If the input is moving the player right and the player is facing left...
-			if (move > 0 && !m_FacingRight)
+			//only control the player if grounded or airControl is turned on
+			if (m_Grounded || m_AirControl)
 			{
-				// ... flip the player.
-				Flip();
-			}
-			// Otherwise if the input is moving the player left and the player is facing right...
-			else if (move < 0 && m_FacingRight)
-			{
-				// ... flip the player.
-				Flip();
-			}
-		}
-		// If the player should jump...
-		if (jump)
-		{
-			Vector2 jumpVector = new Vector2(0f, m_JumpForce);
-			if (m_Grounded && !wasJumping)
-			{
-				// Add a vertical force to the player.
-				m_Grounded = false;
-				m_Rigidbody2D.AddForce(jumpVector);
-			}
-			m_Rigidbody2D.AddForce(jumpVector * inAirJumpRatio);
-		}
+
+				// If crouching
+				if (crouch)
+				{
+					if (!m_wasCrouching)
+					{
+						m_wasCrouching = true;
+						OnCrouchEvent.Invoke(true);
+					}
+
+					// Reduce the speed by the crouchSpeed multiplier
+					move *= m_CrouchSpeed;
+
+					// Disable one of the colliders when crouching
+					if (m_CrouchDisableCollider != null)
+						m_CrouchDisableCollider.enabled = false;
+				}
+				else
+				{
+					// Enable the collider when not crouching
+					if (m_CrouchDisableCollider != null)
+						m_CrouchDisableCollider.enabled = true;
+
+					if (m_wasCrouching)
+					{
+						m_wasCrouching = false;
+						OnCrouchEvent.Invoke(false);
+					}
+
+					if (sprint && m_Grounded)
+					{
+						move *= m_SprintSpeed;
+					}
+				}
 
 
+				// Move the character by finding the target velocity
+				Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+				// And then smoothing it out and applying it to the character
+				m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
+
+				// If the input is moving the player right and the player is facing left...
+				if (move > 0 && !m_FacingRight)
+				{
+					// ... flip the player.
+					Flip();
+				}
+				// Otherwise if the input is moving the player left and the player is facing right...
+				else if (move < 0 && m_FacingRight)
+				{
+					// ... flip the player.
+					Flip();
+				}
+			}
+			// If the player should jump...
+			if (jump)
+			{
+				Vector2 jumpVector = new Vector2(0f, m_JumpForce);
+				if (m_Grounded && !wasJumping)
+				{
+					// Add a vertical force to the player.
+					m_Grounded = false;
+					m_Rigidbody2D.AddForce(jumpVector);
+				}
+				m_Rigidbody2D.AddForce(jumpVector * inAirJumpRatio);
+			}
 	}
 
 
